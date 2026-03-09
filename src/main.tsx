da8b8fb345c5a250ed5843091000ea85a4ece441
iaoficialmarketing <agenciaonlyfxns@gmail.com>
2026-03-09 07:07:15 +0100

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Router from './Router.tsx';
-import CookieBanner from './components/CookieBanner.tsx';
+import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router />
-    <CookieBanner />
+    <App />
   </StrictMode>
 );
